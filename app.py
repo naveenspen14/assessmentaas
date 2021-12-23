2873b020ff22df66d261a791c18a655bb21f8199
Naveen Penumala <46645245+naveenspen14@users.noreply.github.com>
2021-12-23 13:54:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import yaml
 app = Flask(__name__)
 
 # Configure db
-db = yaml.load(open('db.yaml'))
+db = yaml.load(open('database.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
